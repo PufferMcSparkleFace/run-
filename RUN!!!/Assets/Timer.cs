7c0383d5fe9d7bce57fad286b634c02a68aa161d
Daniel Gutierrez <83122636+PufferMcSparkleFace@users.noreply.github.com>
2024-03-12 23:55:34 +1300

    Timer Functioning

---
+++ RUN!!!/Assets/Timer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+
+    public Text timerText;
+    public int remainingTime;
+    public GameOverScreen gameOver;
+
+    public void Start()
+    {
+        StartCoroutine(Countdown());
+    }
+
+    IEnumerator Countdown()
+    {
+        while(remainingTime > 0)
+        {
+            timerText.text = remainingTime.ToString();
+
+            yield return new WaitForSeconds(1f);
+
+            remainingTime--;
+        }
+
+        timerText.text = "";
+        gameOver.GameOver(1);
+
+    }
+
+
+}
