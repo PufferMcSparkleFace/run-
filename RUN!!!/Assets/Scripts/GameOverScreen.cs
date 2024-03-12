7ade2ca8e39ec21a281d98809d1d798e8825b70d
Daniel Gutierrez <83122636+PufferMcSparkleFace@users.noreply.github.com>
2024-03-12 23:24:12 +1300

    Menu Works

---
+++ RUN!!!/Assets/Scripts/GameOverScreen.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameOverScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Text ZombiesWin;
+    public Text SurvivorWins;
+
+    public void GameOver(int winner)
+    {
+        gameObject.SetActive(true);
+        if(winner == 0)
+        {
+            ZombiesWin.gameObject.SetActive(true);
+        }
+        else
+        {
+            SurvivorWins.gameObject.SetActive(true);
+        }
+    }
+
+    public void PlayAgainButton()
+    {
+        Debug.Log("Run it back");
+        SceneManager.LoadScene("Gameplay");
+    }
+
+    public void CharacterSelectButton()
+    {
+        SceneManager.LoadScene("Character Select");
+    }
+
+    public void QuitButton()
+    {
+        Debug.Log("Bye!");
+        Application.Quit();
+    }
+}
