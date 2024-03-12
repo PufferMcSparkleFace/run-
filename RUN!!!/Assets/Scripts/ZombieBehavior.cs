ecab80dbe5562d94861fd54146a855235b7890ac
Daniel Gutierrez <83122636+PufferMcSparkleFace@users.noreply.github.com>
2024-03-13 02:28:12 +1300

    addforce (feels bad, terrible acceleration)

--- RUN!!!/Assets/Scripts/ZombieBehavior.cs
+++ RUN!!!/Assets/Scripts/ZombieBehavior.cs
@@ -9,6 +9,8 @@ public class ZombieBehavior : MonoBehaviour
     public GameOverScreen gameOver;
     public GameObject timerText;
     public Material zombieMaterial;
+    public float moveSpeed = 200;
+    public Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
@@ -19,7 +21,25 @@ public class ZombieBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(moveSpeed * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-moveSpeed * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0, 0, moveSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0, 0, -moveSpeed * Time.deltaTime);
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
