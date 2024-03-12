8f36d7337c10f483556b0c28231534ae75531ca2
Daniel Gutierrez <83122636+PufferMcSparkleFace@users.noreply.github.com>
2024-03-13 02:52:38 +1300

    Survivor movement kinda

--- RUN!!!/Assets/SurvivorBehavior.cs
+++ RUN!!!/Assets/SurvivorBehavior.cs
@@ -7,6 +7,8 @@ public class SurvivorBehavior : MonoBehaviour
 {
     public GameObject timerText;
     public GameOverScreen gameOver;
+    public Rigidbody rb;
+    public float moveSpeed;
 
     // Start is called before the first frame update
     void Start()
@@ -17,7 +19,25 @@ public class SurvivorBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(moveSpeed * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-moveSpeed * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0, 0, moveSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0, 0, -moveSpeed * Time.deltaTime);
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
